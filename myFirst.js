2a4d13c2eb872ff3d8e88044a7b0112d8a9027f4
Jack Wright <jackwright@d142-058-161-070.wireless.sfu.ca>
2018-09-25 22:24:20 -0700

    ajwb

--- myFirst.js
+++ myFirst.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('addHero.html', function (err, html) {
+fs.readFile('ajwbmainsite.html', function (err, html) {
 
     if (err) throw err;
 
